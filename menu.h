0263dff0da9a1773d946e5490e9403c38788d0e5
Sevla8 <44164412+Sevla8@users.noreply.github.com>
2018-12-21 13:25:21 +0100

    Add files via upload

--- menu.h
+++ menu.h
@@ -4,10 +4,13 @@
 #include <graph.h>
 #include <stdlib.h>
 #include "parameter.h"
+#include "defines.h"
+#include "start.h"
 
 char* inttostr(int);
 
 int screen_1(S_parameter*);
+void screen_help();
 int screen_2(S_parameter*);
 int screen_3(S_parameter*);
 int screen_4_1(S_parameter*);
